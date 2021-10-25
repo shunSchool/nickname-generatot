8ec7786cc8e88e2881c08f8b99bd434e6fdaf348
shunSchool <72032942+shunSchool@users.noreply.github.com>
2021-10-25 11:27:38 -0600

    finished

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //event listener for buttons
-document.getElementById("Random nickname").addEventListener("click", buttonHandler)
-document.getElementById("All names").addEventListener("click", buttonHandler)
+document.getElementById("Random nickname").addEventListener("click", randomButtonHandler)
+document.getElementById("All names").addEventListener("click", allButtonHandler)
 
 // Global Variables
 let nicknames;
@@ -17,12 +17,22 @@ function processData(stringData) {
 
 //input names and generate nickname
 
-
-function buttonHandler () {
+function randomButtonHandler () {
     //input
     let firstName = document.getElementById("firstName").value;
     let lastName = document.getElementById("lastName").value;
     //output
     let randomNickname = nicknames[randomInt(0, nicknames.length)]
-    document.getElementById("nickname").innerHTML = (firstName + randomNickname + lastName)
+    document.getElementById("nickname").innerHTML = `${firstName} "${randomNickname}" ${lastName}`;
+}
+
+function allButtonHandler () {
+    //input
+    let firstName = document.getElementById("firstName").value;
+    let lastName = document.getElementById("lastName").value;
+
+    for (let i = 0; i < nicknames.length; i++) {
+        document.getElementById("nickname").innerHTML += `${firstName} "${nicknames[i]}" ${lastName}<br>`;
+    }
+    
 }
